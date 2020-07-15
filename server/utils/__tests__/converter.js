185ba5a65ac6f7e8f7e065e512aff6418b5b187e
hieptien0512 <68257921+hieptien0512@users.noreply.github.com>
2020-07-15 14:53:37 +0700

    Update converter.js

--- server/utils/__tests__/converter.js
+++ server/utils/__tests__/converter.js
@@ -3,3 +3,10 @@ const converter = require("../converter") ;
 test('convert F To C', function () {
     expect(converter.convertFToC(32)).toBe(0);
 });
+
+test('convert F To K', function () {
+    expect(converter.convertFToK(0)).toBeCloseTo(255.37);
+});
+test('convert F To K', function () {
+    expect(converter.convertFToK(-50)).toBeCloseTo(227.59);
+});
