81a7d9bb053c97ea33ee9430e60bc03977ccbb9e
hieptien0512 <68257921+hieptien0512@users.noreply.github.com>
2020-07-16 10:42:41 +0700

    update validate

--- server/utils/__tests__/converter.js
+++ server/utils/__tests__/converter.js
@@ -7,11 +7,29 @@ test("convert F To C", function () {
 test("convert 0 C To K", function () {
   expect(converter.convertCToK(0)).toBeCloseTo(273.15);
 });
+
+test("convert 0 C To K", function () {
+  expect(converter.convertCToK(-300)).toBeCloseTo(false);
+});
+
+test("convert 0 C To K", function () {
+  expect(converter.convertCToK(-273.15)).toBeCloseTo(0);
+});
+
 test("convert F To K", function () {
   expect(converter.convertFToK(0)).toBeCloseTo(255.37);
 });
+
 test("convert F To K", function () {
   expect(converter.convertFToK(-50)).toBeCloseTo(227.59);
+});
+
+test("convert F To K", function () {
+  expect(converter.convertFToK(-459.67)).toBeCloseTo(false);
+});
+
+test("convert F To K", function () {
+  expect(converter.convertFToK(-459.65)).toBeCloseTo(0);
 });
 
 test("convert 30 C To K", function () {
